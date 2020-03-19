f34efedc54b865dfafd8d00f508ebed60a716abe
amandajdecker <amanda.decker@gmail.com>
2020-03-18 20:20:56 -0400

    Add scroll to top button logic

--- script.js
+++ script.js
@@ -1,3 +1,5 @@
+// Burger Menu
+
 const burgerMenuButton = document.getElementById("burger-menu");
 const menuLinkWrapper = document.getElementById("menu-link-wrapper");
 let burgerMenuIsOpen = false;
@@ -13,3 +15,36 @@ burgerMenuButton.addEventListener("click", function() {
         burgerMenuIsOpen = false;
     }
 });
+
+// Scroll to top
+
+const scrollTopButton = document.getElementById("scroll-top-button");
+let scrollPosition = window.scrollTop
+
+scrollTopButton.addEventListener("click", function() {
+    window.scrollTo({
+        top: 0,
+        behavior: "smooth"
+    })
+});
+
+// scrollTopButton.addEventListener("scroll", function() {
+//     if (scrollPosition > 50) {
+//         scrollTopButton.classList.remove("hide-scroll-button");
+//     } else {
+//         scrollTopButton.classList.add("hide-scroll-button");
+//     }
+// });
+
+// let prevScrollPos = window.pageYOffset;
+
+const topOfPage = 800
+
+window.onscroll = function() {
+    let currentScrollPos = window.pageYOffset;
+    if (currentScrollPos > topOfPage) {
+        document.getElementById("scroll-top-button").classList.remove("hide-scroll-button");
+    } else {
+        document.getElementById("scroll-top-button").classList.add("hide-scroll-button");
+    }
+};
